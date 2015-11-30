c677bcacb55fd1b2896028ed5106258bd295fc5d
Vladimir <vgrebenuk@positrace.com>
2015-11-30 14:32:03 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial::Application.config.secret_token = 'ba878f2af7548ee6c04c2e9538ea38205b784641025a8fbf3782c693aa1c7a48be46ae3ce2b55d5cf5387c08afa03e15085dfcf546a398221b02cc62e919cd0a'
