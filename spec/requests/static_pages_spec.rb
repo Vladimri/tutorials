b5de068b8072156083a059a12f88999d7848fa05
Vladimir <vgrebenuk@positrace.com>
2015-12-01 15:25:19 +0200

    text

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,39 @@
 require 'spec_helper'
 
+describe "Static pages" do
   describe "Home page" do
     it "should have content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
-describe "Help page" do
-  it "should have content 'Help'" do
-    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    visit '/static_pages/help'
-    expect(page).to have_content('Help')
+  describe "Help page" do
+    it "should have content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
-end
 
-describe "About us page" do
-  it "should have content 'About us'" do
-    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    visit '/static_pages/help'
-    expect(page).to have_content('About us')
+  describe "About us page" do
+    it "should have content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 end
