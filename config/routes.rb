04051d3e52b8864510a93a5f8170155d49e1bcb5
Vladimir <vgrebenuk@positrace.com>
2015-12-03 09:28:36 +0200

    text

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Tutorial::Application.routes.draw do
 
   root to: 'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
